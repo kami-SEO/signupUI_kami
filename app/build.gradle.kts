17f8b010b1f241dd2b2908c35fb93ff4a383d082
SKM <rkdalsv@naver.com>
2023-12-20 14:20:04 +0900

    아이디입력속성hint/input type

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.signupui_kami"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.signupui_kami"
         minSdk = 16
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
